30ad22afc6d75e7beee60c901404f7bf19902e21
KG <kgitts98@gmail.com>
2023-01-14 12:04:42 -0800

    .

--- two_sum.py
+++ two_sum.py
@@ -2,7 +2,9 @@ from typing import List
 
 class Solution:
   def twoSum(self, nums: List[int], target: int) -> List[int]:
-    pass
+    for i in nums:
+      if nums[i] + nums[i+1] == 9:
+        return "yes"
 
 solution = Solution()
-print(solution.twoSum())
+print(solution.twoSum([2, 7, 11, 15], 9))
