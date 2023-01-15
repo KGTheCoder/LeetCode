32f4da9cf69f33fb26a0d345135aeece3f427ff3
KG <kgitts98@gmail.com>
2023-01-15 07:58:34 -0800

    .

--- palidrome.py
+++ palidrome.py
@@ -1,10 +1,39 @@
 class Solution:
   def isPalindrome(self, x: int) -> bool:
     # return str(x[::1]) == str(x)
+
+    """
     if x < 0:
       return False
     
     return str(x) == str(x)[::-1]
+    """
+
+    """
+    if x < 0:
+      return False
+    
+    inputNum = x
+    newNum = 0
+
+    while x > 0:
+      newNum = newNum * 10 + x % 10
+      x = x // 10
+    return newNum == inputNum
+    """
+
+    if x < 0:
+      return False
+
+    a, b = x, 0
+
+    while x:
+      b *= 10
+      b += x % 10
+      x //= 10
+
+    return a == b
+
 
 
 solution = Solution()
