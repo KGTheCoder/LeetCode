3a725065a43a95468cf53ce2b6dd8c93a5f7841f
KG <kgitts98@gmail.com>
2023-01-21 07:52:21 -0800

    .

--- mySqrt.py
+++ mySqrt.py
@@ -3,10 +3,24 @@
 class Solution:
   def mySqrt(self, x: int) -> int:
     # return int(math.sqrt(x))
+
+    """
     index = 1
     while index * index <= x:
       index += 1
     return index - 1
+    """
+
+    if x == 0:
+      return 0
+    for i in range(1, x + 1):
+      if i * i == x:
+        return i
+      elif i * i > x:
+        return (i-1)
+
+
+
 
 sol = Solution()
 print(sol.mySqrt(8))
