22c2d8d1c2ce5590e73e832dd47224b2ef80f8f0
KG <kgitts98@gmail.com>
2023-01-22 14:36:55 -0800

    .

---
+++ plusOne.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+  def plusOne(self, digits: List[int]) -> List[int]:
+    """
+    n = len(digits)
+
+    for i in range(n):
+      idx = n - 1 - i
+      if digits[idx] == 9:
+        digits[idx] = 0
+      else:
+        digits[idx] += 1
+        return digits
+  
+    return [1] + digits
+    """
+
+    num = 0
+    for i in range(len(digits)):
+      num += digits[i] * pow(10, (len(digits)-1-i))
+    return [int(i) for i in str(num+1)]
+
+
+sol = Solution()
+print(sol.plusOne([1, 2, 3]))
--- roman_to_int.py
+++ roman_to_int.py
@@ -1,5 +1,6 @@
 class Solution:
   def romanToInt(self, s: str) -> int:
+    """
     roman_num = {
       "I": 1,
       "V": 5,
@@ -24,6 +25,27 @@ class Solution:
     for char in s:
       number += roman_num[char]
     return number 
+    """
+
+    roman_num = {
+      "I": 1,
+      "V": 5,
+      "X": 10,
+      "L": 50,
+      "C": 100,
+      "D": 500,
+      "M": 1000
+    }
+
+    output = 0
+
+    for i in s[::-1]:
+      if 4 * roman_num[i] > output:
+        output += roman_num[i]
+      else:
+        output -= roman_num[i]
+    
+    return output
 
 sol = Solution()
-print(sol.romanToInt("III"))
+print(sol.romanToInt("MXCMCIV"))
