495cd9c68e4ecea8aba17ed5be535e4ca8d28ee7
KG <kgitts98@gmail.com>
2023-01-28 13:19:30 -0800

    .

--- plusOne.py
+++ plusOne.py
@@ -64,7 +64,7 @@ class Solution:
     return [int(i) for i in a]
     """
 
-    return [int(i) for i in str(int(''.join([str(i) for i in digits])) + 1)]
+    # return [int(i) for i in str(int(''.join([str(i) for i in digits])) + 1)]
 
 
 
